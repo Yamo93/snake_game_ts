fcd207c9e8ad2eb09c5cc570a35c8a1a2e5409d0
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2023-06-17 23:24:08 +0200

    create game loop

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,33 @@
-let isWorldSaved = false;
-export function saveTheWorld() {
-    if (isWorldSaved) {
-        return `Too late, world has already been saved!`;
-    }
-    else {
-        isWorldSaved = true;
-        return `Hurray, you just saved the world!`;
-    }
+"use strict";
+function createCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("width", "400");
+    canvas.setAttribute("height", "400");
+    document.body.append(canvas);
+    return canvas;
 }
-console.log("snake game");
+const canvas = createCanvas();
+const SNAKE_SPEED = 2;
+const TILE_COUNT = 20;
+let lastRenderTime = 0;
+function gameLoop(currentRenderTime) {
+    window.requestAnimationFrame(gameLoop);
+    const timeSinceLastRenderInSeconds = (currentRenderTime - lastRenderTime) / 1000;
+    if (timeSinceLastRenderInSeconds < 1 / SNAKE_SPEED)
+        return;
+    lastRenderTime = currentRenderTime;
+    update();
+    draw();
+}
+window.requestAnimationFrame(gameLoop);
+function update() { }
+function draw() {
+    clearScreen();
+}
+function clearScreen() {
+    const context = canvas.getContext("2d");
+    if (!context)
+        throw new Error("2d context missing");
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,40 @@
-let isWorldSaved: boolean = false;
-
-export function saveTheWorld(): string {
-  if (isWorldSaved) {
-    return `Too late, world has already been saved!`;
-  } else {
-    isWorldSaved = true;
-    return `Hurray, you just saved the world!`;
-  }
+function createCanvas(): HTMLCanvasElement {
+  const canvas = document.createElement("canvas");
+  canvas.setAttribute("width", "400");
+  canvas.setAttribute("height", "400");
+  document.body.append(canvas);
+  return canvas;
 }
 
-console.log("snake game");
+const canvas = createCanvas();
+
+const SNAKE_SPEED = 2;
+const TILE_COUNT = 20;
+let lastRenderTime = 0;
+
+function gameLoop(currentRenderTime: number) {
+  window.requestAnimationFrame(gameLoop);
+  const timeSinceLastRenderInSeconds =
+    (currentRenderTime - lastRenderTime) / 1000;
+  if (timeSinceLastRenderInSeconds < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentRenderTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(gameLoop);
+
+function update() {}
+
+function draw() {
+  clearScreen();
+}
+
+function clearScreen() {
+  const context = canvas.getContext("2d");
+  if (!context) throw new Error("2d context missing");
+  context.fillStyle = "black";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
